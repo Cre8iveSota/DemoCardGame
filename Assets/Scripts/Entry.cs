27287059df05091aac47ec18bb0e7bac72371376
SotaHiguchi <sota.higuchi@student.torrens.edu.au>
2024-01-01 20:39:51 +1100

    add Scriptable Onject

---
+++ Assets/Scripts/Entry.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Entry", menuName = "CreateEmpty")]
+public class Entry : ScriptableObject
+{
+    public int hp;
+    public int at;
+
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private GameObject panel;
+    [SerializeField] private Entry entry;
     void Start()
     {
         panel.SetActive(true);
+        Debug.Log("EntryAt " + entry.at);
+        Debug.Log("EntryHP  " + entry.hp);
     }
 
 }
