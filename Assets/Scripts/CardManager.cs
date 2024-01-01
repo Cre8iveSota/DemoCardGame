b3221916a2cd3c4c138fa624449405dc9fd7d37c
SotaHiguchi <sota.higuchi@student.torrens.edu.au>
2024-01-01 20:50:42 +1100

    add card movement by do tween

--- Assets/Scripts/CardManager.cs
+++ Assets/Scripts/CardManager.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class CardManager : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] Transform endPoint;
     void Start()
     {
-
+        transform.DOMove(endPoint.position, 2f);
     }
 }
